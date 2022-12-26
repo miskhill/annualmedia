62df75461d5230b04a439567f96b8572bd4bb1a7
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-26 11:05:48 +0000

    series models

---
+++ server/models/series.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+const SeriesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "can't be blank"],
+    unique: true,
+    index: true,
+    maxlength: 100,
+  },
+  year: {
+    type: Number,
+    required: [true, "can't be blank"],
+    index: true,
+  },
+  genre: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+    maxlength: 100,
+  },
+  director: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+    maxlength: 100,
+  },
+  actors: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+    maxlength: 100,
+  },
+  plot: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+    maxlength: 100,
+  },
+  poster: {
+    type: String,
+    required: [true, "can't be blank"],
+    index: true,
+    maxlength: 100,
+  },
+  website: {
+    type: String,
+    index: true,
+    maxlength: 100,
+  },
+}, { timestamps: true });
+
+export default mongoose.model('Series', SeriesSchema);
