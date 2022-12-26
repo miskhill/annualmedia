01f60ffb99222e4ba75ea531478064ed01cd6afa
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-26 14:55:48 +0000

    Router movies series books added

--- config/router.js
+++ config/router.js
@@ -2,4 +2,20 @@ import express from 'express';
 import { getAllMovies, createMovie, getMovieById } from '../server/controllers/moviesController';
 import { getAllBooks, createBook, getBookById } from '../server/controllers/booksController';
 import { getAllSeries, getSeriesById, createSeries } from '../server/controllers/seriesController';
+import { secureRoute } from './secureRoute';
 
+const router = express.Router();
+
+router.get('/movies', getAllMovies);
+router.get('/movies/:id', getMovieById);
+router.post('/movies', secureRoute, createMovie);
+
+router.get('/books', getAllBooks);
+router.get('/books/:id', getBookById);
+router.post('/books', secureRoute, createBook);
+
+router.get('/series', getAllSeries);
+router.get('/series/:id', getSeriesById);
+router.post('/series', secureRoute, createSeries);
+
+export default router;
