bc034e0965b224de992544c4f80e8c1602a22f17
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-26 14:36:36 +0000

    added controllers for books series movies

---
+++ server/controllers/moviesController.js
@@ -0,0 +1,36 @@
+import movies from '../models/movies';
+
+export const getAllMovies = async (req, res) => {
+  try {
+    const movies = await movies.find();
+    res.status(200).json(movies);
+  }
+  catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const getMovieById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movie = await
+      movies.findById
+        (id);
+    res.status(200).json(movie);
+
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const createMovie = async (req, res) => {
+  const { body } = req;
+  const newMovie = new movies(body);
+  try {
+    await newMovie.save();
+    res.status(201).json(newMovie);
+  }
+  catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+}
---
+++ server/controllers/seriesController.js
@@ -0,0 +1,36 @@
+import series from '../models/series';
+
+export const getAllSeries = async (req, res) => {
+  try {
+    const series = await series.find();
+    res.status(200).json(series);
+  }
+  catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const getSeriesById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const series = await
+      series.findById
+        (id);
+    res.status(200).json(series);
+  }
+  catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
+
+export const createSeries = async (req, res) => {
+  const { body } = req;
+  const newSeries = new series(body);
+  try {
+    await newSeries.save();
+    res.status(201).json(newSeries);
+  }
+  catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+}
