1175f79e2b26b07ce30bf4a47a67415e25baaa1a
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-26 10:56:35 +0000

    user model

---
+++ server/models/user.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';  
+import uniqueValidator from 'mongoose-unique-validator';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "can't be blank"],
+    unique: true,
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true,
+    maxlength: 20,
+  },  
+  email: {
+    type: String,
+    required: [true, "can't be blank"],
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+  },
+  password: {
+    type: String,
+    required: [true, "can't be blank"],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+}, { timestamps: true });
+
+UserSchema.pre('validate', function (next) {
+  if (!this.username) {
+    this.username = this.email;
+  }
+  next();
+});
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = bcrypt.hashSync(this.password, bcrypt.getSaltSync(10));
+  next();
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+export default mongoose.model('User', UserSchema);
