039869cdbd86eb147c482973bd06e15372fde9cc
Gary Smith <80961839+miskhill@users.noreply.github.com>
2022-12-29 15:10:50 +0000

    added front end get method

--- src/components/importCard.js
+++ src/components/importCard.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl"
+import axios from 'axios'
 
 
 
 // create a reusable card to allow a user to import movie details
 const ImportCard = () => {
 
+  // get a movie document from the database
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async () => {
+    try {
+      const { data } = await axios.get('/api/movies')
+      setMovie(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  getMovie()
+  
 return (
 
   <form >
