19d05e473d08206962fb9f619c481945eab16725
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-01-15 11:48:41 +0000

    remove scruffy commented code

--- src/components/importCard.js
+++ src/components/importCard.js
@@ -3,33 +3,7 @@ import MovieUploadCard from "./upload/movieUploadCard";
 import SeriesUploadCard from "./upload/seriesUploadCard";
 
 const ImportCard = () => {
-  // const mystyle = {
-  //   color: "white",
-  //   backgroundColor: "DodgerBlue",
-  //   padding: "10px",
-  //   fontFamily: "Arial",
-  //   display: "flex",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   flexDirection: "column",
-  // };
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm();
-
-  // const onSubmitMovie = (movie) => {
-  //   axios
-  //     .post("/api/movies", movie)
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
 
   return (
     <>
