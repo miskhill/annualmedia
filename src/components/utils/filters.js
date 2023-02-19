b4131065dca066250abb5bdde1d46304de74c296
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-02-19 11:40:47 +0000

    Inline css for search bar

--- src/components/utils/filters.js
+++ src/components/utils/filters.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Filters = ({ handleFilterChange, handleSortBy, sortBy, searchTerm }) => {
   return (
     <>
-      <div className="filters d-flex justify-content-around" id="filtersDiv">
-        <input onChange={ handleFilterChange } name="searchTerm" value={ searchTerm } placeholder=' search media'/>
-        <select onChange={handleSortBy} name="sortBy" value={sortBy}>
-          <option value="createdAt">Year</option>
-          <option value="title">Title</option>
-          <option value="views">Author</option>
-          <option value="rating">Genre</option>
-          
+      <div
+        id='filtersDiv'
+        style={{
+          margin: "0 auto",
+          maxWidth: 800,
+          backgroundColor: "#3b3835",
+        }}
+      >
+        <input
+          style={{
+            width: "200px",
+            height: "26px",
+            backgroundColor: "#fff",
+            color: "#000",
+            opacity: '1',
+            fontWeight: '800',
+          }}
+          onChange={handleFilterChange}
+          name='searchTerm'
+          value={searchTerm}
+          placeholder='Search Media 📖'
+        />
+        <select style={{
+          border: '0',
+          position: 'absolute',
+          borderLeft: '2px solid skyblue',
+          height: '26px',
+          fontWeight: '900',
+          fontSize: '12px',
+          backgroundColor: '#f6f5ef',
+          color:'#009F8A'
+        } } onChange={handleSortBy} name='sortBy' value={sortBy}>
+          <option value='createdAt'>Year</option>
+          <option value='title'>Title</option>
+          <option value='views'>Author</option>
+          <option value='rating'>Genre</option>
         </select>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Filters
+export default Filters;
