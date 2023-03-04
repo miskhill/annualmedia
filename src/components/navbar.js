7988bca1b05a5bc9d21db0b0b206ddaf40fe2792
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-03-04 17:04:37 +0000

    changed navbar and drawer component so i can remove mui

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,71 +1,39 @@
-    import React from "react";
-    import {
-      AppBar,
-      Toolbar,
-      CssBaseline,
-      Typography,
-      makeStyles,
-      useTheme,
-      useMediaQuery,
-    } from "@material-ui/core";
-    import { Link } from "react-router-dom";
-    import DrawerComponent from "./drawer";
-    
-    const useStyles = makeStyles((theme) => ({
-      navlinks: {
-        marginLeft: theme.spacing(5),
-        display: "flex",
-      },
-      logo: {
-        flexGrow: "1",
-        cursor: "pointer",
-      },
-      link: {
-        textDecoration: "none",
-        color: "white",
-        fontSize: "20px",
-        marginLeft: theme.spacing(20),
-        "&:hover": {
-          color: "yellow",
-          borderBottom: "1px solid white",
-        },
-      },
-    }));
-    
-    function Navbar() {
-      const classes = useStyles();
-      const theme = useTheme();
-      const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-    
-      return (
-        <AppBar position="static">
-          <CssBaseline />
-          <Toolbar>
-            <Typography variant="h4" className={classes.logo}>
-              Annual Media
-              
-            </Typography>
-            {isMobile ? (
-              <DrawerComponent />
-            ) : (
-              <div className={classes.navlinks}>
-                <Link to="/" className={classes.link}>
-                  Upload
-                </Link>
-                <Link to="/movies" className={classes.link}>
-                  Movies
-                </Link>
-                <Link to="/series" className={classes.link}>
-                  Series
-                </Link>
-                <Link to="/books" className={classes.link}>
-                  Books
-                </Link>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      );
-    }
+import { useState } from "react"
+import "../styles/navbar.css"
 
-    export default Navbar;
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Annual Media
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        {/* hamburger svg code... */}
+      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/movies">Movies</a>
+          </li>
+          <li>
+            <a href="/series">Series</a>
+          </li>
+          <li>
+            <a href="/books">Books</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
