ee528a87a6e6c0266d59f3c68c82041efeb8b5a9
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-03-11 11:57:34 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 import router from '../config/router.js';
-import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
