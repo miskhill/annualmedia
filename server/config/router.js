95a37f590b2db0f2e56ac18ee605942ce3492c6b
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-03-11 12:51:53 +0000

    moved routes folder into the server folder

---
+++ server/config/router.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {
+  getAllMovies,
+  createMovie,
+  getMovieById,
+} from "../controllers/moviesController.js";
+import {
+  getAllBooks,
+  createBook,
+  getBookById,
+} from "../controllers/booksController.js";
+import {
+  getAllSeries,
+  getSeriesById,
+  createSeries,
+} from "../controllers/seriesController.js";
+// import { secureRoute } from './secureRoute.js';
+
+//TO DO - add secureRoute to all create routes
+//TO DO - add secureRoute to all delete routes and create those routes
+
+const router = express.Router();
+
+router.get("/movies", getAllMovies);
+router.get("/movies/:id", getMovieById);
+router.post("/movies", createMovie);
+
+router.get("/books", getAllBooks);
+router.get("/books/:id", getBookById);
+router.post("/books", createBook);
+
+router.get("/series", getAllSeries);
+router.get("/series/:id", getSeriesById);
+router.post("/series", createSeries);
+
+export default router;
--- server/index.js
+++ server/index.js
@@ -1,41 +1,40 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import router from '../config/router.js';
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import router from "./config/router.js";
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.REACT_APP_PORT;
-console.log(port)
+console.log(port);
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.REACT_APP_DB_URI)
-    console.log('MongoDB connected ⭐️')
+    await mongoose.connect(process.env.REACT_APP_DB_URI);
+    console.log("MongoDB connected ⭐️");
 
     app.use(express.json());
-    app.use('/api', router);
+    app.use("/api", router);
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 
     app.use((req, _res, next) => {
-      console.log(`Request received: ${req.method} - ${req.url}`)
-      next()
-    })
+      console.log(`Request received: ${req.method} - ${req.url}`);
+      next();
+    });
 
     app.use((_req, res) => {
-      return res.status(404).json({ message: 'Path not found' })
-    })
+      return res.status(404).json({ message: "Path not found" });
+    });
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
   }
 };
