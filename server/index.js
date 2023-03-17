e71647c66d68e3388e6f4f5e0d766f28b67ee928
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-03-17 09:34:10 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import router from "./config/router.js";
-// import path, { dirname } from "path";
-// import { fileURLToPath } from 'url';
+import path, { dirname } from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 // const path = require('path')
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const port = process.env.REACT_APP_PORT;
 // const host = process.env.REACT_APP_HOST;
 console.log(port, 'using this port');
@@ -23,10 +23,10 @@ const startServer = async () => {
     app.use(express.json());
     app.use("/api", router);
 
-    // app.get("*", (_req, res) => {
-    //   console.log(path, 'path');
-    //   res.sendFile(path.join(__dirname, "index.html"));
-    // });
+    app.get("*", (_req, res) => {
+      console.log(path, 'path');
+      res.sendFile(path.join(__dirname, "index.html"));
+    });
 
     app.use((req, _res, next) => {
       console.log(`Request received: ${req.method} - ${req.url}`);
