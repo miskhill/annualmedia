666e11c16f824069ba3f46f74a8580514adc2a09
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-03-17 12:47:37 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 // const __filename = fileURLToPath(import.meta.url)
 // const __dirname = dirname(__filename)
 const port = process.env.REACT_APP_PORT;
-// const host = process.env.REACT_APP_HOST;
+const host = process.env.REACT_APP_HOST;
 console.log(port, 'using this port');
 
 const startServer = async () => {
@@ -53,7 +53,7 @@ const startServer = async () => {
       return res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404'});
     });
 
-    app.listen(port,() => {
+    app.listen(port, host,() => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (err) {
