928724621ebc7c9da79165160459d0a257c7b146
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-04-09 10:08:07 +0100

    last change!

--- src/components/books.js
+++ src/components/books.js
@@ -2,19 +2,21 @@ import { useState, useEffect } from "react";
 import MediaCard from "./card.js";
 import Filters from "./utils/filters.js";
 import axios from "axios";
-import AnnualTotals from './utils/annualTotals.js';
+import AnnualTotals from "./utils/annualTotals.js";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
-  const [ searchBooks, setSearchBooks ] = useState([])
-  const [ filters, setFilters ] = useState({ searchTerm: '' })
-  const [ sortBy, setSortBy ] = useState('createdAt')
-  const [ sortedArray, setSortedArray ] = useState([])
+  const [searchBooks, setSearchBooks] = useState([]);
+  const [filters, setFilters] = useState({ searchTerm: "" });
+  const [sortBy, setSortBy] = useState("createdAt");
+  const [sortedArray, setSortedArray] = useState([]);
 
   useEffect(() => {
     const getBooks = async () => {
       try {
-        const { data } = await axios.get("https://annualmediaserver.onrender.com/api/books");
+        const { data } = await axios.get(
+          "https://annualmediaserver.onrender.com/api/books"
+        );
         console.log(data, "render data");
         setBooks(Object.values({ ...data }));
       } catch (err) {
@@ -35,40 +37,52 @@ const Books = () => {
   };
 
   const whichSort = (array, sortBy) => {
-    if (sortBy === 'Genre' || sortBy === 'Title' || sortBy === 'CreatedAt' || sortBy === 'Author') {
-      return array.sort((a,b)=> (a[sortBy] < b[sortBy] ? 1 : -1))
-    } if 
-      (sortBy === 'Year' || sortBy === 'Rating'){
-        return array.sort((a,b)=> (a[sortBy] > b[sortBy] ? -1 : 1))
-      }
-    
-    else {
-      return array.sort((a,b)=> (a[sortBy] > b[sortBy] ? 1 : -1))
+    if (
+      sortBy === "Genre" ||
+      sortBy === "Title" ||
+      sortBy === "CreatedAt" ||
+      sortBy === "Author"
+    ) {
+      return array.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
     }
-  }
+    if (sortBy === "Year" || sortBy === "Rating") {
+      return array.sort((a, b) => (a[sortBy] < b[sortBy] ? -1 : 1));
+    } else {
+      return array.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+    }
+  };
 
   useEffect(() => {
-    console.log(sortBy, 'sortBy')
-    setSortedArray(whichSort(books, sortBy))
-  }, [sortBy, books])
+    console.log(sortBy, "sortBy");
+    setSortedArray(whichSort(books, sortBy));
+  }, [sortBy, books]);
 
   useEffect(() => {
-    const regexSearch = new RegExp(filters.searchTerm, 'i')
-    setSearchBooks((sortedArray ? sortedArray : whichSort(books, sortBy)).filter(book => {
-      return regexSearch.test(book.title)
-    }))
-  }, [filters, sortBy, sortedArray, books])
+    const regexSearch = new RegExp(filters.searchTerm, "i");
+    setSearchBooks(
+      (sortedArray ? sortedArray : whichSort(books, sortBy)).filter((book) => {
+        return regexSearch.test(book.title);
+      })
+    );
+  }, [filters, sortBy, sortedArray, books]);
 
   return (
     <>
-      
-      <Filters id="matchesFilters" handleFilterChange={handleFilterChange} handleSortBy={handleSortBy} {...filters}/>
+      <Filters
+        id='matchesFilters'
+        handleFilterChange={handleFilterChange}
+        handleSortBy={handleSortBy}
+        {...filters}
+      />
       <div className='totals'>
-      <h3> You have read <AnnualTotals arr={books} year={2023} /> books this year</h3>
+        <h3>
+          {" "}
+          You have read <AnnualTotals arr={books} year={2023} /> books this year
+        </h3>
       </div>
-      <div className="matchesGrid">
-        { (filters.searchTerm !== '' ? searchBooks : sortedArray ).map(book => { 
-          return <MediaCard key={book.id} image={book.poster} { ...book } />
+      <div className='matchesGrid'>
+        {(filters.searchTerm !== "" ? searchBooks : sortedArray).map((book) => {
+          return <MediaCard key={book.id} image={book.poster} {...book} />;
         })}
       </div>
     </>
