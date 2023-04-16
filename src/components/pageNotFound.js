1942134c80488977f75c2573758930f688397d81
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-04-16 13:02:30 +0100

    Update pageNotFound.js

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const PageNotFound = () => {
 
-  // return a 404 page layout include a link to the home page and the vamp404 image from the public folder
-
   return (
     <div className="pageNotFound">
-      <h1>404 Page Not Found</h1>
+      <h1>Page Not Found</h1>
       <img src="/vamp404.png" alt="404 Page Not Found" />
       <p>
         <a href="/">Go Home</a>
