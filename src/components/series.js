6444ea6c043b62c03a4193c885eb90fad2c536ce
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-04-23 11:56:18 +0100

    Update series.js

--- src/components/series.js
+++ src/components/series.js
@@ -1,53 +1,3 @@
-// import { useState, useEffect } from "react";
-// import MediaCard from "./card.js";
-// import axios from "axios";
-// import AnnualTotals from "./utils/annualTotals.js";
-
-// const Series = () => {
-//   const [series, setSeries] = useState([]);
-
-//   useEffect(() => {
-//     try {
-//       axios
-//         .get("https://annualmediaserver.onrender.com/api/series")
-//         .then((res) => {
-//           setSeries(res.data);
-//           console.log(res.data, "render data");
-//         });
-//     } catch (err) {
-//       console.log(err, "catch error");
-//     }
-//   }, []);
-
-//   return (
-//     <>
-//       <div className='totals'>
-//         <h3>
-//           {" "}
-//           You have watched <AnnualTotals arr={series} year={2023} /> series this
-//           year
-//         </h3>
-//       </div>
-//       <div>
-//         {series.map((series) => {
-//           return (
-//             <MediaCard
-//               key={series.id}
-//               title={series.title}
-//               year={series.year}
-//               genre={series.genre}
-//               rating={series.rating}
-//               image={series.poster}
-
-//             />
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Series;
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MediaCard from "./card.js";
@@ -79,9 +29,11 @@ const Series = () => {
     return () => clearTimeout(timer);
   };
 
-  const filteredSeries = series.filter((s) => {
-    return s.title.toLowerCase().includes(searchTerm.toLowerCase());
+  const filteredSeries = series.filter((series) => {
+    return series.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
+
+  const uniqueSeries = [...new Map(filteredSeries.map(item => [item.id, item])).values()];
 
   return (
     <>
@@ -89,46 +41,31 @@ const Series = () => {
         <input type="text" onChange={debounceSearch} />
         <button>Search</button>
         <h3>
-          {" "}
           display search results for {searchTerm}, showing{" "}
-          {filteredSeries.length}
-          {filteredSeries.length === 1 ? "series" : "series"}
-          {filteredSeries.length === 0 && searchTerm !== "" && (
+          {uniqueSeries.length}
+          {uniqueSeries.length === 1 ? " series" : " series"}
+          {uniqueSeries.length === 0 && searchTerm !== "" && (
             <p>No series found</p>
           )}
         </h3>
       </div>
       <div className="totals">
         <h3>
-          You have watched{" "}
-          <AnnualTotals arr={series} year={2023} /> series this year
+          You have watched <AnnualTotals arr={series} year={2023} /> series
+          this year
         </h3>
       </div>
       <div>
-        {series.map((s) => {
-          if (searchTerm !== "") {
-            return (
-              <MediaCard
-                key={filteredSeries[0].id}
-                title={filteredSeries[0].title}
-                year={filteredSeries[0].year}
-                genre={filteredSeries[0].genre}
-                rating={filteredSeries[0].rating}
-                image={filteredSeries[0].poster}
-              />
-            );
-          }
-          return (
-            <MediaCard
-              key={s.id}
-              title={s.title}
-              year={s.year}
-              genre={s.genre}
-              rating={s.rating}
-              image={s.poster}
-            />
-          );
-        })}
+        {uniqueSeries.map((series) => (
+          <MediaCard
+            key={series.id}
+            title={series.title}
+            year={series.year}
+            genre={series.genre}
+            rating={series.rating}
+            image={series.poster}
+          />
+        ))}
       </div>
     </>
   );
