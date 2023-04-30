e3aa9a86e1a041589f6a576c25c6924be1da4be4
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-04-30 15:19:51 +0100

    loading state added to movies

--- src/components/movies.js
+++ src/components/movies.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from "react";
 import MediaCard from "./card.js";
 import axios from "axios";
 import AnnualTotals from "./utils/annualTotals.js";
+// import the loading gif from public folder
+
 import Grid from "@mui/material/Grid";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredMovies, setFilteredMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     try {
       axios
         .get("https://annualmediaserver.onrender.com/api/movies")
         .then((res) => {
           setMovies(res.data);
+          setLoading(false)
           console.log(res.data, "render data");
         });
     } catch (err) {
@@ -65,33 +70,37 @@ const Movies = () => {
         </h3>
       </div>
 
-      <Grid container spacing={3}>
-        {searchTerm !== ""
-          ? filteredMovies.map((movie) => (
-              <Grid item xs={12} sm={6} md={4} key={movie.id}>
-                <MediaCard
-                  title={movie.title}
-                  year={movie.year}
-                  genre={movie.genre}
-                  rating={movie.rating}
-                  image={movie.poster}
-                  plot={movie.plot}
-                />
-              </Grid>
-            ))
-          : movies.map((movie) => (
-              <Grid item xs={12} sm={6} md={4} key={movie.id}>
-                <MediaCard
-                  title={movie.title}
-                  year={movie.year}
-                  genre={movie.genre}
-                  rating={movie.rating}
-                  image={movie.poster}
-                  plot={movie.plot}
-                />
-              </Grid>
-            ))}
-      </Grid>
+      {loading ? ( // Check loading state variable
+        <img src="loading.gif" alt="Loading" /> // Display loading text or loading gif here
+      ) : (
+        <Grid container spacing={3}>
+          {searchTerm !== ""
+            ? filteredMovies.map((movie) => (
+                <Grid item xs={12} sm={6} md={4} key={movie.id}>
+                  <MediaCard
+                    title={movie.title}
+                    year={movie.year}
+                    genre={movie.genre}
+                    rating={movie.rating}
+                    image={movie.poster}
+                    plot={movie.plot}
+                  />
+                </Grid>
+              ))
+            : movies.map((movie) => (
+                <Grid item xs={12} sm={6} md={4} key={movie.id}>
+                  <MediaCard
+                    title={movie.title}
+                    year={movie.year}
+                    genre={movie.genre}
+                    rating={movie.rating}
+                    image={movie.poster}
+                    plot={movie.plot}
+                  />
+                </Grid>
+              ))}
+        </Grid>
+      )}
     </>
   );
 };
