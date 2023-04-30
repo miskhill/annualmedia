ba16fcd30a7a53e4967764c29b402ed18aec75cf
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-04-30 12:58:08 +0100

    removed unused code

--- src/components/series.js
+++ src/components/series.js
@@ -1,100 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import MediaCard from "./card.js";
-// import axios from "axios";
-// import AnnualTotals from "./utils/annualTotals.js";
-
-// const Series = () => {
-//   const [series, setSeries] = useState([]);
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const [filteredSeries, setFilteredSeries] = useState([]);
-
-//   useEffect(() => {
-//     try {
-//       axios
-//         .get("https://annualmediaserver.onrender.com/api/series")
-//         .then((res) => {
-//           setSeries(res.data);
-//           console.log(res.data, "render data");
-//         });
-//     } catch (err) {
-//       console.log(err, "catch error");
-//     }
-//   }, []);
-
-//   // refactor handleSearch to debounce the typed in result
-
-//   const debounceSearch = (e) => {
-//     const searchTerm = e.target.value;
-//     const timer = setTimeout(() => {
-//       setSearchTerm(searchTerm);
-//     }, 1500);
-//     console.log(timer, "timer");
-//     return () => clearTimeout(timer);
-//   };
-
-//   useEffect(() => {
-//     setFilteredSeries(
-//       Array.from(new Set(series.filter((serie) =>
-//         serie.title.toLowerCase().includes(searchTerm.toLowerCase())
-//       ).map(serie => serie.title)))
-//       .map(title => series.find(serie => serie.title === title))
-//     );
-//   }, [series, searchTerm]);
-
-//   return (
-//     <>
-//       <div className='search' style={{ backgroundColor: '#8c1839', color: '#ffffff' }}>
-//   <input type='text' onChange={debounceSearch} style={{ backgroundColor: '#ffffff', color: '#000000' }} />
-//   <button style={{ backgroundColor: '#ffffff', color: '#8c1839' }}>Search</button>
-//   <h3>
-//     {" "}
-//     display search results for {searchTerm}, showing{" "}
-//     {searchTerm !== "" ? filteredSeries.length : series.length}
-//     {searchTerm !== "" && (filteredSeries.length === 1 ? " series" : " series")}
-
-//     {filteredSeries.length === 0 && searchTerm !== "" && (
-//       <p>No series found</p>
-//     )}
-//   </h3>
-// </div>
-
-//       <div className='totals'>
-//         <h3>
-//           {" "}
-//           You have watched <AnnualTotals arr={series} year={2023} /> series this
-//           year
-//         </h3>
-//       </div>
-
-//       <div>
-//         {(searchTerm !== ""
-//           ? filteredSeries.map((serie) => (
-//               <MediaCard
-//                 key={serie.id}
-//                 title={serie.title}
-//                 year={serie.year}
-//                 genre={serie.genre}
-//                 rating={serie.rating}
-//                 image={serie.poster}
-//               />
-//             ))
-//           : series.map((serie) => (
-//               <MediaCard
-//                 key={serie.id}
-//                 title={serie.title}
-//                 year={serie.year}
-//                 genre={serie.genre}
-//                 rating={serie.rating}
-//                 image={serie.poster}
-//               />
-//             )))}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Series;
-
 import React, { useState, useEffect } from "react";
 import MediaCard from "./card.js";
 import axios from "axios";
@@ -132,24 +35,38 @@ const Series = () => {
 
   useEffect(() => {
     setFilteredSeries(
-      Array.from(new Set(series.filter((serie) =>
-        serie.title.toLowerCase().includes(searchTerm.toLowerCase())
-      ).map(serie => serie.title)))
-      .map(title => series.find(serie => serie.title === title))
+      Array.from(
+        new Set(
+          series
+            .filter((serie) =>
+              serie.title.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((serie) => serie.title)
+        )
+      ).map((title) => series.find((serie) => serie.title === title))
     );
   }, [series, searchTerm]);
 
   return (
     <>
-      <div className='search' style={{ backgroundColor: '#8c1839', color: '#ffffff' }}>
-        <input type='text' onChange={debounceSearch} style={{ backgroundColor: '#ffffff', color: '#000000' }} />
-        <button style={{ backgroundColor: '#ffffff', color: '#8c1839' }}>Search</button>
+      <div
+        className='search'
+        style={{ backgroundColor: "#8c1839", color: "#ffffff" }}
+      >
+        <input
+          type='text'
+          onChange={debounceSearch}
+          style={{ backgroundColor: "#ffffff", color: "#000000" }}
+        />
+        <button style={{ backgroundColor: "#ffffff", color: "#8c1839" }}>
+          Search
+        </button>
         <h3>
           {" "}
           display search results for {searchTerm}, showing{" "}
           {searchTerm !== "" ? filteredSeries.length : series.length}
-          {searchTerm !== "" && (filteredSeries.length === 1 ? " series" : " series")}
-
+          {searchTerm !== "" &&
+            (filteredSeries.length === 1 ? " series" : " series")}
           {filteredSeries.length === 0 && searchTerm !== "" && (
             <p>No series found</p>
           )}
@@ -165,7 +82,7 @@ const Series = () => {
       </div>
 
       <Grid container spacing={2}>
-        {(searchTerm !== ""
+        {searchTerm !== ""
           ? filteredSeries.map((serie) => (
               <Grid item xs={12} sm={6} md={4} lg={3}>
                 <MediaCard
@@ -175,6 +92,7 @@ const Series = () => {
                   genre={serie.genre}
                   rating={serie.rating}
                   image={serie.poster}
+                  plot={serie.plot}
                 />
               </Grid>
             ))
@@ -187,13 +105,13 @@ const Series = () => {
                   genre={serie.genre}
                   rating={serie.rating}
                   image={serie.poster}
+                  plot={serie.plot}
                 />
               </Grid>
-            )))}
+            ))}
       </Grid>
     </>
   );
 };
 
 export default Series;
-
