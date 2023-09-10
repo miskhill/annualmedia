96043214350e237ad839b7bf03bacf0584b5ff8f
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-09-10 14:34:13 +0100

    drop down to filter by movie year watched and show only those

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,198 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import MediaCard from "./card.js";
-// import axios from "axios";
-// import AnnualTotals from "./utils/annualTotals.js";
-// import Filter from "./utils/filter.js";
-// import Grid from "@mui/material/Grid";
-
-// const Movies = () => {
-//   const [movies, setMovies] = useState([]);
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const [filteredMovies, setFilteredMovies] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [filters, setFilters] = useState({
-//     genres: [],
-//     ratings: [],
-//     years: [],
-//     createdAts: [],
-//   });
-//   const [selectedFilters, setSelectedFilters] = useState({
-//     genre: "",
-//     rating: "",
-//     year: "",
-//     createdAt: "",
-//   });
-
-//   useEffect(() => {
-//     setLoading(true);
-//     try {
-//       axios
-//         .get("https://annualmediaserver.onrender.com/api/movies")
-//         .then((res) => {
-//           setMovies(res.data);
-//           setLoading(false);
-//           console.log(res.data, "render data");
-//         });
-//     } catch (err) {
-//       console.log(err, "catch error");
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     const filtered = movies.filter((movie) => {
-//       return (
-//         movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-//         (selectedFilters.genre === "" ||
-//           movie.genre.includes(selectedFilters.genre)) &&
-//         (selectedFilters.rating === "" ||
-//           movie.rating === selectedFilters.rating) &&
-//         (selectedFilters.year === "" ||
-//           movie.year === parseInt(selectedFilters.year)) &&
-//         (selectedFilters.createdAt === "" ||
-//           movie.createdAt === selectedFilters.createdAt)
-//       );
-//     });
-//     setFilteredMovies(filtered);
-//   }, [movies, searchTerm, selectedFilters]);
-
-//   // refactor handleSearch to debounce the typed in result
-
-//   const debounceSearch = (e) => {
-//     const searchTerm = e.target.value;
-//     const timer = setTimeout(() => {
-//       setSearchTerm(searchTerm);
-//     }, 500);
-//     console.log(timer, "timer");
-//     return () => clearTimeout(timer);
-//   };
-
-//   // useEffect(() => {
-//   //   setFilteredMovies(
-//   //     movies.filter((movie) =>
-//   //       movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-//   //     )
-//   //   );
-//   // }, [movies, searchTerm]);
-
-//   return (
-//     <>
-//       <div
-//         style={{
-//           display: "flex",
-//           alignItems: "center",
-//           justifyContent: "center",
-//           marginBottom: "20px",
-//         }}
-//         className='search'
-//       >
-//         <input
-//           style={{
-//             width: "250px",
-//             height: "30px",
-//             borderRadius: "5px",
-//             border: "1px solid #ccc",
-//             padding: "5px 10px",
-//             fontSize: "16px",
-//             marginRight: "10px",
-//           }}
-//           type='text'
-//           onChange={debounceSearch}
-//         />
-//         <button
-//           style={{
-//             backgroundColor: "#e50914",
-//             color: "#fff",
-//             border: "none",
-//             borderRadius: "5px",
-//             padding: "5px 10px",
-//             fontSize: "16px",
-//             cursor: "pointer",
-//           }}
-//         >
-//           Search
-//         </button>
-//         {searchTerm !== "" && (
-//           <h3 style={{ fontSize: "20px", fontWeight: "bold", margin: "0" }}>
-//             {" "}
-//             display search results for {searchTerm}, showing{" "}
-//             {filteredMovies.length}{" "}
-//             {filteredMovies.length === 1 ? "movie" : "movies"}
-//           </h3>
-//         )}
-//       </div>
-
-//       <div
-//         style={{
-//           display: "flex",
-//           alignItems: "center",
-//           justifyContent: "center",
-//           marginBottom: "20px",
-//           textAlign: "center",
-//         }}
-//         className='totals'
-//       >
-//         <h3 style={{ fontSize: "20px", fontWeight: "bold", margin: "0" }}>
-//           {" "}
-//           You have watched <AnnualTotals arr={movies} year={2023} /> movies this
-//           year
-//         </h3>
-//       </div>
-
-//       <Filter
-//         filters={filters}
-//         selectedFilters={selectedFilters}
-//         onChange={(filterType, value) =>
-//           setSelectedFilters({ ...selectedFilters, [filterType]: value })
-//         }
-//         mediaType='movies'
-//       />
-
-//       {loading ? (
-//         <div
-//           className='loading-container'
-//           style={{
-//             display: loading ? "flex" : "none",
-//             justifyContent: "center",
-//             alignItems: "center",
-//           }}
-//         >
-//           <img src='loading.gif' alt='Loading' />
-//         </div>
-//       ) : (
-//         <Grid container spacing={3}>
-//           {searchTerm !== ""
-//             ? filteredMovies.map((movie) => (
-//                 <Grid item xs={12} sm={6} md={4} key={movie.id}>
-//                   <MediaCard
-//                     title={movie.title}
-//                     year={movie.year}
-//                     genre={movie.genre}
-//                     rating={movie.rating}
-//                     image={movie.poster}
-//                     plot={movie.plot}
-//                   />
-//                 </Grid>
-//               ))
-//             : movies.map((movie) => (
-//                 <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
-//                   <MediaCard
-//                     title={movie.title}
-//                     year={movie.year}
-//                     genre={movie.genre}
-//                     rating={movie.rating}
-//                     image={movie.poster}
-//                     plot={movie.plot}
-//                   />
-//                 </Grid>
-//               ))}
-//         </Grid>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Movies;
-
 import React, { useState, useEffect } from "react";
 import MediaCard from "./card.js";
 import axios from "axios";
@@ -206,6 +11,8 @@ const Movies = () => {
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [sortBy, setSortBy] = useState("createdAt");
+  const [selectedYear, setSelectedYear] = useState(2023);
+
 
   useEffect(() => {
     setLoading(true);
@@ -249,12 +56,13 @@ const Movies = () => {
           return (
             movie.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
             movie.genre.toLowerCase().includes(searchTerm.toLowerCase())
-          );
+          ) && movie.year === selectedYear;
         }),
         sortBy
       )
     );
-  }, [movies, searchTerm, sortBy]);
+  }, [movies, searchTerm, sortBy, selectedYear]);
+  
 
   return (
     <>
@@ -276,7 +84,7 @@ const Movies = () => {
       >
         <h3 style={{ fontSize: "20px", fontWeight: "bold", margin: "0" }}>
           {" "}
-          You have watched <AnnualTotals arr={movies} year={2023} /> movies this
+          You have watched <AnnualTotals arr={movies} year={selectedYear} handleYearChange={setSelectedYear} /> movies this
           year
         </h3>
       </div>
--- src/components/utils/annualTotals.js
+++ src/components/utils/annualTotals.js
@@ -1,22 +1,32 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
 
-const AnnualTotals = ({ arr, year }) => {
+const AnnualTotals = ({ arr, year, handleYearChange }) => {
+  const [inputYear, setInputYear] = useState(year);
 
   const annualTotals = (arr, year) => {
     let total = 0;
     arr.forEach((item) => {
       if (item.createdAt.slice(0, 4) === year.toString()) {
-        total ++;
+        total++;
       }
-    // total++
     });
     return total;
-  }
+  };
+
+  useEffect(() => {
+    handleYearChange(inputYear);
+  }, [inputYear, handleYearChange]);
 
   return (
-      annualTotals(arr, year)
+    <div>
+      <input
+        type="number"
+        value={inputYear}
+        onChange={(e) => setInputYear(e.target.value)}
+      />
+      {annualTotals(arr, inputYear)}
+    </div>
   );
-}
+};
 
 export default AnnualTotals;
-
