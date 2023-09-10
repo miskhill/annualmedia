f05fdfeb0f85cb475ca64ae1d36983a92515d1fd
Gary Smith <80961839+miskhill@users.noreply.github.com>
2023-09-10 15:02:45 +0100

    Update annualTotals.js

--- src/components/utils/annualTotals.js
+++ src/components/utils/annualTotals.js
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const AnnualTotals = ({ arr, year, handleYearChange }) => {
   const [inputYear, setInputYear] = useState(year);
 
+  const handleInputChange = (e) => {
+    setInputYear(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleYearChange(inputYear);
+  };
+
   const annualTotals = (arr, year) => {
     let total = 0;
-    arr.forEach((movie) => {
-      if (movie.createdAt && movie.createdAt.slice(0, 4) === year.toString()) {
+    arr.forEach((item) => {
+      if (item.createdAt.slice(0, 4) === year.toString()) {
         total++;
       }
     });
     return total;
   };
 
-  useEffect(() => {
-    handleYearChange(inputYear);
-  }, [inputYear, handleYearChange]);
-
   return (
     <div>
-      <input
-        type="number"
-        value={inputYear}
-        onChange={(e) => setInputYear(e.target.value)}
-      />
-      {annualTotals(arr, inputYear)}
+      <form onSubmit={handleSubmit}>
+        <input type="number" value={inputYear} onChange={handleInputChange} />
+        <button type="submit">Update Year</button>
+      </form>
+      {annualTotals(arr, year)}
     </div>
   );
 };
