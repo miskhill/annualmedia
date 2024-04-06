f7862d208981d93d90f6ed8b983e0240ac60bd9a
Gary Smith <80961839+miskhill@users.noreply.github.com>
2024-04-06 11:00:15 +0100

    edit MUI padding iphone11

--- src/components/bookGrid.js
+++ src/components/bookGrid.js
@@ -3,7 +3,7 @@ import MediaCard from "./card.js";
 
 const BookGrid = ({ books }) => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} padding={'20px'}>
       {books.map((book) => (
         <Grid item xs={12} sm={6} md={4} lg={3} key={book.id}>
           <MediaCard image={book.poster} {...book} />
